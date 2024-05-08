0d4d44d08fb8d3e607ff72bc0cd42140bf348b00
Douglas Mora <douglas-mora@outlook.com>
2024-05-08 11:11:19 -0600

    Adding tokenization

---
+++ app/oauth2.py
@@ -0,0 +1,42 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from schemas import TokenData
+from fastapi import status, HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+SECRET_KEY = "lk2345kj324jhDFQWrq1!#adsfDh345JII)ihhBH#3DbE5$7Ha3efddf"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data:dict):
+    to_encode = data.copy()
+    
+    expire = datetime.now(datetime.UTC) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp":expire})
+    
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    
+    return encoded_jwt
+
+
+def verify_access_token(token:str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id:str=payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+    
+    return token_data
+        
+        
+def get_current_user(token:str=Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WWW-AUTHENTICATE":"Bearer"})
+    
+    return verify_access_token(token, credentials_exception)
--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
+from typing import Optional
 
 class PostBase(BaseModel):
     "This is the schema that defines the post model"
@@ -32,3 +33,18 @@ class UserResponse(BaseModel):
     
     class Config:
         from_attributes = True
+        
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+    
+    
+class Token(BaseModel):
+    access_token = str
+    token_type = str
+    
+    
+class TokenData(BaseModel):
+    id = Optional[str]=None
+    
